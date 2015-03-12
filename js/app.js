3d45e98efe863cf05fdd1d37e0e2dc36820b8c9a
Alexis <alexis.phanor@gmail.com>
2015-03-12 20:49:25 +0000

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var scannerapp = angular.module('scannerapp', [
+	'ngRoute',
+	'scannerappControllers'
+]);
+
+var scannerappControllers = angular.module('scannerappControllers', []);
+
+scannerapp.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.
+		when('/results', {
+		    templateUrl: 'views/results.html'
+	}).
+	otherwise({
+		redirectTo: '/'
+	})
+	;
+}]);
